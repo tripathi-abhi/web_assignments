f91bf8a0638968ba3a0e7d909d41bbbc0c9b6eac
unknown <abhitripathi2312gmail.com>
2020-07-18 15:16:55 +0530

    assignment4 done

--- assignment_3/js/script.js
+++ assignment_3/js/script.js
@@ -1,12 +1,19 @@
-function Dog(name) {
-  this.name = name;
-  console.log(this);
-}
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-Dog.prototype.bark = function () {
-      console.log(this);
-  console.log(this.name + " likes barking! Bark!");
+// STEP 10:
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// See Lecture 50, part 1
+for (var name in names) {
+  
+  // STEP 11:
+  // Retrieve the first letter of the current name in the loop.
+  // Use the string object's 'charAt' function. Since we are looking for
+  // names that start with either upper case or lower case 'J'/'j', call
+  // string object's 'toLowerCase' method on the result so we can compare
+  // to lower case character 'j' afterwards.
+  // Look up these methods on Mozilla Developer Network web site if needed.
+  var firstLetter = names[name].charAt(0);
+  console.log(firstLetter);
 }
-
-var max = new Dog("Max", "Buddy");
-max.bark();
